1a1a23758a1633a923c0040d6351e7f27d1adfd4
vberger <vberger@users.noreply.github.com>
2021-03-01 14:38:53 +0000

    deploy: ce223343e5ec9da42237862d399f54b72fbbebda

--- implementors/futures_io/if_std/trait.AsyncWrite.js
+++ implementors/futures_io/if_std/trait.AsyncWrite.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["calloop"] = [{"text":"impl&lt;'l, F:&nbsp;AsRawFd + Write&gt; AsyncWrite for Async&lt;'l, F&gt;","synthetic":false,"types":[]}];
+implementors["calloop"] = [{"text":"impl&lt;'l, F:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/unix/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::unix::ext::io::AsRawFd\">AsRawFd</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; AsyncWrite for <a class=\"struct\" href=\"calloop/io/struct.Async.html\" title=\"struct calloop::io::Async\">Async</a>&lt;'l, F&gt;","synthetic":false,"types":["calloop::io::Async"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
