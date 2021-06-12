a0215fb54952b9123f207036691b8cae8cfb68ab
detly <detly@users.noreply.github.com>
2021-06-12 04:54:07 +0000

    deploy: 8638c67369b9c9ff80a18959d93852fdf27aa658

--- source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["calloop"] = {"name":"","dirs":[{"name":"sources","files":["channel.rs","futures.rs","generic.rs","mod.rs","ping.rs","signals.rs","timer.rs"]},{"name":"sys","files":["epoll.rs","mod.rs"]}],"files":["io.rs","lib.rs","list.rs","loop_logic.rs"]};
-createSourceSidebar();
